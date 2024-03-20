7659c0adaf469a400220f07fb7e50e050cea88c8
Mauro <81922575+MaSeraphina@users.noreply.github.com>
2024-03-20 08:29:43 +0100

    Aanpassing van de getters en setters
    + opkuis van de layout, meeste is hetzelfde gebleven.

--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -4,10 +4,7 @@ public class Speler {
 	private String gebruikersnaam;
 	private int geboortejaar;
 	private int aantalGewonnen, aantalGespeeld;
-
-	public Speler(String gebruikersnaam, int geboortejaar) {
-		this(gebruikersnaam, geboortejaar, 0, 0);
-	}
+	private String kleur;
 
 	public Speler(String gebruikersnaam, int geboortejaar, int aantalGewonnen, int aantalGespeeld) {
 		setGebruikersnaam(gebruikersnaam);
@@ -16,36 +13,60 @@ public class Speler {
 		setAantalGespeeld(aantalGespeeld);
 	}
 
-	public String getGebruikersnaam() {
-		return gebruikersnaam;
+	public Speler(String gebruikersnaam, int geboortejaar) {
+		this(gebruikersnaam, geboortejaar, 0, 0);
 	}
+
+	// Setters
 
 	private void setGebruikersnaam(String gebruikersnaam) {
 		this.gebruikersnaam = gebruikersnaam;
-	}
-
-	public int getGeboortejaar() {
-		return geboortejaar;
 	}
 
 	private void setGeboortejaar(int geboortejaar) {
 		this.geboortejaar = geboortejaar;
 	}
 
-	public int getAantalGewonnen() {
-		return aantalGewonnen;
-	}
-
 	private void setAantalGewonnen(int aantalGewonnen) {
 		this.aantalGewonnen = aantalGewonnen;
+	}
+
+	private void setAantalGespeeld(int aantalGespeeld) {
+		this.aantalGespeeld = aantalGespeeld;
+	}
+
+	private void setKleur(String kleur) {
+		this.kleur = kleur;
+	}
+
+	// Getters
+
+	public String getGebruikersnaam() {
+		return gebruikersnaam;
+	}
+
+	public int getGeboortejaar() {
+		return geboortejaar;
+	}
+
+	public int getAantalGewonnen() {
+		return aantalGewonnen;
 	}
 
 	public int getAantalGespeeld() {
 		return aantalGespeeld;
 	}
 
-	private void setAantalGespeeld(int aantalGespeeld) {
-		this.aantalGespeeld = aantalGespeeld;
+	public String getKleur() {
+		return kleur;
+	}
+
+	public void heeftGespeelt() {
+
+	}
+
+	public void heeftGewonnen() {
+
 	}
 
 }
