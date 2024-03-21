47a03cc4453dcbe10e8fb2571dfc2421db6feb9c
Xander Beuselinck <xander.beuselinck@student.hogent.be>
2024-03-21 11:53:52 +0100

    Skelet domeinklasse tegel

--- src/domein/Tegel.java
+++ src/domein/Tegel.java
@@ -2,4 +2,14 @@ package domein;
 
 public class Tegel {
 
+	private String landschap;
+	private int kroontjes;
+	public Tegel(String landschap, int kroontjes) {
+	
+		this.landschap = landschap;
+		this.kroontjes = kroontjes;
+	}
+	public String tegel(String landschap) {
+		return landschap;
+	}
 }
