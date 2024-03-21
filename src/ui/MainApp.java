c317159a458f9d9021d094cba0f18e7ec70426d0
Xander Beuselinck <xander.beuselinck@student.hogent.be>
2024-03-21 15:13:43 +0100

    error messages in main app

--- src/ui/MainApp.java
+++ src/ui/MainApp.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import domein.DomeinController;
@@ -26,21 +27,51 @@ public class MainApp {
 
 	private String vraagNaarNaam() {
 		String naam;
-		do {
-			System.out.print("Naam speler: ");
-			naam = invoer.nextLine();
-		} while (naam == null || naam.isBlank()); // TODO error toevoegen
+		boolean correct = false;
+		do {// probeerd blok of invulling naam voldoet aan parrameters in if blok zo niet
+			// throw passende melding
+			try {
+				System.out.print("Naam speler: ");
+				naam = invoer.nextLine();
+				if (naam == null || naam.isBlank()) {
+					throw new IllegalArgumentException("naam kan enkel letters bevatten.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("geen geldige String ingevuld. Porbeer opnieuw.");
+				return vraagNaarNaam();
+			} catch (IllegalArgumentException e) {
+				System.err.println(e.getMessage());
+				return vraagNaarNaam();
+			}
+			correct = true;
+		} while (!correct);
 		return naam;
 	}
 
 	private int vraagNaarGeboortjaar() {
-		int jaar;
+		byte jaar;
+		boolean correct = false;
 		do {
-			System.out.print("Geboortejaar: ");
-			jaar = invoer.nextInt();
-		} while (jaar < 1900 || jaar > 2024); // TODO error toevoegen
+			try {
+				// probeerd blok of invulling jaar voldoet aan parrameters in if blok zo niet
+				// throw passende melding
+				System.out.print("Geboortejaar: ");
+				jaar = invoer.nextByte();
+				if (jaar < 1900 || jaar > 2024) {
+					throw new IllegalArgumentException("Geboortejaar moet tussen 1900 en 2024 liggen.");
+				}
+			} catch (InputMismatchException e) {
 
+				System.err.println("Geen geldig getal ingevoerd. Probeer opnieuw.");
+				return vraagNaarGeboortjaar();
+			} catch (IllegalArgumentException e) {
+				System.err.println(e.getMessage());
+				return vraagNaarGeboortjaar();
+			}
+			correct = true;
+		} while (!correct);
 		return jaar;
+
 	}
 
 }
