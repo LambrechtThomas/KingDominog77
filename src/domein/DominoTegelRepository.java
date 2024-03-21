fa151b49fd43dd52e207074077590e5fb0efc744
Maxence Joosten <maxence.joosten@student.hogent.be>
2024-03-21 14:40:27 +0100

    DominoTegelRepository aangevuld
    Alle methodes aangemaakt

--- src/domein/DominoTegelRepository.java
+++ src/domein/DominoTegelRepository.java
@@ -1,12 +1,39 @@
 package domein;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DominoTegelRepository {
-	private DominoTegel[] alleDominos;
+	private ArrayList<DominoTegel> alleDominos;
 
 	public DominoTegelRepository() {
 	}
 	
-	public 
+	// Schud het deck
+	public void schud() {
+		Collections.shuffle(alleDominos);
+	}
+	
+	// Returnt de eerste domino op het deck
+	public DominoTegel geefDomino() {
+		DominoTegel eersteTegel = alleDominos.get(0);
+		alleDominos.remove(0);
+		return eersteTegel; 
+	}
+	
+	// Returnt volledige lijst van dominos
+	public ArrayList<DominoTegel> geefLijstDominos() {
+		return alleDominos;
+	}
+
+	// Geeft een lijst van domino's terug (aan de hand van de aantal spelers)
+	public ArrayList<DominoTegel> geefKolom(int aantalSpelers) {
+		ArrayList<DominoTegel> dominos = new ArrayList<DominoTegel>();
+		for(int i = 0; i < aantalSpelers; i++) {
+			dominos.add(geefDomino());
+		}
+		return dominos;
+	}
 	
 	
 }
