9e2db61739b1fa90f44cd86d05f97ea314277a5d
RobinS45 <robin.sonck@student.hogent.be>
2024-03-21 14:02:57 +0100

    Update DominoTegel.java
    toevoegen methodes en constructoren aan DominoTegel.

--- src/domein/DominoTegel.java
+++ src/domein/DominoTegel.java
@@ -4,9 +4,16 @@ public class DominoTegel {
 	private int volgnummer;
 	private boolean horizontaal;
 	private boolean spiegeld;
-	
+
 	public DominoTegel(int volgnummer) {
-		
+		this.volgnummer = volgnummer;
+	}
+
+	public void draai() {
+
+	}
+
+	public void spiegel() {
+
 	}
 }
-
