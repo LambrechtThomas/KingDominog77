094fef6caec9c4cd7b69aff45c01e9e6b5322139
Mauro <81922575+MaSeraphina@users.noreply.github.com>
2024-03-28 20:45:57 +0100

    Exceptions toegevoegd in Tegel.java

--- src/domein/Tegel.java
+++ src/domein/Tegel.java
@@ -2,22 +2,30 @@ package domein;
 
 public class Tegel {
 
+	private static final int MIN_AANTAL_KROONTJES = 0;
+	private static final int MAX_AANTAL_KROONTJES = 3;
+
 	private final String landschap;
 	private final int kroontjes;
-	
+
 	public Tegel(String landschap, int kroontjes) {
+
+		if (kroontjes < MIN_AANTAL_KROONTJES || kroontjes > MAX_AANTAL_KROONTJES)
+			throw new IllegalArgumentException(String.format("Een tegel moet tussen %d en %d kroontjes hebben",
+					MIN_AANTAL_KROONTJES, MAX_AANTAL_KROONTJES));
+
 		this.landschap = landschap;
 		this.kroontjes = kroontjes;
 	}
-	
+
 	public Tegel() {
-		//Deze constructor wordt gebruikt zodat "KasteelTegel" kan overerven
-		//Vul attributen aan zodat we weten dat het de kasteel is
+		// Deze constructor wordt gebruikt zodat "KasteelTegel" kan overerven
+		// Vul attributen aan zodat we weten dat het de kasteel is
 		this.landschap = "Kasteel";
 		this.kroontjes = 0;
 	}
-	
-	//Getters
+
+	// Getters
 	public String getLandschap() {
 		return landschap;
 	}
