7a349f86772b6b6e47a6523dbdb52fff2c34d0e2
Thomas Lambrecht <Thomas.Lambrecht2@student.hogent.be>
2024-04-25 14:05:18 +0200

    De MainApp
    Nog fouten in

--- src/ui/MainApp.java
+++ src/ui/MainApp.java
@@ -2,12 +2,14 @@ package ui;
 
 import java.time.Year;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
+import DTO.spelerDTO;
 import domein.DomeinController;
 import domein.Kleur;
-import domein.Speler;
 
 public class MainApp {
 
@@ -37,7 +39,7 @@ public class MainApp {
 			if (keuze == 2) {
 				System.out.println("\n==Speel Spel==");
 				System.out.printf("Er zijn volgende spelers beschikbaar: %s", dc.geefBeschikbareSpelers());
-				
+
 				keuze = keuzeMenu();
 			}
 		}
@@ -63,6 +65,137 @@ public class MainApp {
 		} while (keuze < 1 || keuze > 3);
 
 		return keuze;
+	}
+
+	private void startSpel() {
+		System.out.println("\n==Speel Spel==");
+
+		System.out.print("Met hoeveel wenst u te spelen? ");
+		int hoeveelSpelers = vraagEenGetal(3, 4);
+
+		for (int i = 0; i < hoeveelSpelers; i++) {
+			System.out.printf("Speler %d", i + 1);
+			dc.spelerDoetMee(kiesEenSpeler(), kiesEenKleur());
+		}
+
+		dc.startSpel();
+
+		System.out.println(dc.geefDeelnemendeSpelers());
+
+		while (!dc.isSpelTenEinde()) {
+			System.out.printf("%nHet is ronde %d%n", dc.getRonde());
+
+			speelRonde();
+
+		}
+
+	}
+
+	private void speelRonde() {
+		ArrayList<dominoTegelDTO> startKolom = dc.geefStartKolom();
+		HashMap<spelerDTO, dominoTegelDTO> keuzes = new HashMap<>();
+		int aantalSpelers = dc.geefAantalSpelers();
+
+		for (int i = 0; i < aantalSpelers; i++) {
+			spelerDTO koning = dc.geefKoning();
+
+			System.out.printf("Het is aan %s: %n", koning.gebruikersnaam());
+			int keuze = printDominos(startKolom);
+
+			keuzes.put(koning, startKolom.stream().filter(v -> v.volgnummer() == keuze).findFirst().get());
+			startKolom.remove(startKolom.stream().filter(v -> v.volgnummer() == keuze).findFirst().get());
+
+			dc.kiesNieuweKoning();
+		}
+
+		bevestiging(keuzes);
+
+	}
+
+	private void bevestiging(HashMap<spelerDTO, dominoTegelDTO> keuzes) {
+		System.out.printf("%n%n%nDit is een bevestiging:%n");
+		for (Map.Entry<spelerDTO, dominoTegelDTO> entry : keuzes.entrySet()) {
+			System.out.printf("Voor %s is het %s%n", entry.getKey().gebruikersnaam(), entry.getValue().volgnummer());
+		}
+
+	}
+
+	private int printDominos(ArrayList<dominoTegelDTO> startKolom) {
+		ArrayList<Integer> mogelijkeKeuzes = new ArrayList<>();
+		Collections.sort(startKolom, new dominoTegelComparator());
+
+		for (dominoTegelDTO domino : startKolom) {
+			mogelijkeKeuzes.add(domino.volgnummer());
+			System.out.printf("%-2d: ╔═══════╦═══════╗ %n     %-7d %-7d  %n     %-7s %-7s  %n    ╚═══════╩═══════╝ %n",
+					domino.volgnummer(), domino.tegels()[0].getKroontjes(), domino.tegels()[1].getKroontjes(),
+					domino.tegels()[0].getLandschap(), domino.tegels()[1].getLandschap());
+		}
+
+		int getal;
+		boolean fout = false;
+
+		do {
+			if (fout)
+				System.out.println("\nGelieve een geldige getal in te geven");
+
+			System.out.printf("Maak uw keuze: ");
+			getal = input.nextInt();
+
+			fout = true;
+		} while (!mogelijkeKeuzes.contains(getal));
+
+		return getal;
+
+	}
+
+	private spelerDTO kiesEenSpeler() {
+		System.out.printf("\nDe volgende spelers zijn beschikbaar: \n");
+
+		System.out.println("0: Registreer nieuwe speler");
+
+		int teller = 1;
+		for (spelerDTO speler : beschikbareSpelers) {
+			System.out.printf("%d: Naam: %-18s Geboortedatum: %d %n", teller++, speler.gebruikersnaam(),
+					speler.geboortejaar());
+		}
+
+		int gekozenSpelerIndex = vraagEenGetal(0, beschikbareSpelers.size());
+
+		if (gekozenSpelerIndex == 0) {
+			startRegistratie();
+			return kiesEenSpeler();
+		}
+
+		spelerDTO gekozenSpeler = beschikbareSpelers.get(--gekozenSpelerIndex);
+		beschikbareSpelers.remove(gekozenSpelerIndex);
+
+		return gekozenSpeler;
+	}
+
+	private Kleur kiesEenKleur() {
+
+		System.out.printf("%nDe volgende kleuren zijn beschikbaar: %n");
+		int teller = 1;
+		for (Kleur kleur : beschikbareKleueren) {
+			System.out.printf("%d: %s %n", teller++, kleur);
+		}
+
+		int gekozenKleur = vraagEenGetal(1, beschikbareKleueren.size());
+		Kleur kleur = beschikbareKleueren.get(--gekozenKleur);
+		beschikbareKleueren.remove(gekozenKleur);
+
+		return kleur;
+	}
+
+	private void startRegistratie() {
+		System.out.println("\n==Registeer een Speler==");
+		String gebruikersnaam = vraagGebruikersnaam();
+		int geboortedatum = vraagGebrooteDatum();
+
+		dc.registreerSpeler(gebruikersnaam, geboortedatum);
+
+		System.out.println("\n\nGelukt");
+		beschikbareSpelers = dc.geefBeschikbareSpelers();
 	}
 
 	private String vraagGebruikersnaam() {
@@ -99,6 +232,23 @@ public class MainApp {
 		} while (gebrootedatum < Year.now().getValue() - 121 || gebrootedatum > Year.now().getValue());
 
 		return gebrootedatum;
+	}
+	
+	public int vraagEenGetal(int ondergrens, int bovengrens) {
+		int getal;
+		boolean fout = false;
+
+		do {
+			if (fout)
+				System.out.println("\nGelieve een geldige getal in te geven");
+
+			System.out.printf("Maak uw keuze: ");
+			getal = input.nextInt();
+
+			fout = true;
+		} while (getal < ondergrens || getal > bovengrens);
+
+		return getal;
 	}
 
 }
