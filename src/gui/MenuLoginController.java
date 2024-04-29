b1cf5304f41826b5a8b7188e098050e52f220737
Mauro <81922575+MaSeraphina@users.noreply.github.com>
2024-04-29 14:44:29 +0200

    Voor taal aanpassen
    Voor Maxence

--- src/gui/MenuLoginController.java
+++ src/gui/MenuLoginController.java
@@ -3,10 +3,14 @@ package gui;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MenuLoginController {
@@ -14,6 +18,30 @@ public class MenuLoginController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+
+	@FXML
+	private Button btnLogin;
+
+	@FXML
+	private Button btnNaarStart;
+
+	@FXML
+	private TextField fldGeboortedatum;
+
+	@FXML
+	private TextField fldGebruikersnaam;
+
+	@FXML
+	private Label lblGeboortedatum;
+
+	@FXML
+	private Label lblGebruikersnaam;
+
+	@FXML
+	private Label lblLogInOfMaakAccountAan;
+
+	@FXML
+	private Label lblWelkcomTerug;
 
 	public void switchToSceneStart(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("menuStart.fxml"));
